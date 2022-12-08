5f6d2cfe19013e90570a30626486ce71b5b612a4
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-12-08 09:04:38 +0100

    fixed jwt verification.

--- rest_api/src/auth.rs
+++ rest_api/src/auth.rs
@@ -1,14 +1,30 @@
 use hmac::{Hmac, Mac};
-use jwt::VerifyWithKey;
+use jwt::{AlgorithmType, Header, Token, VerifyWithKey};
 use sha2::Sha256;
 use std::collections::BTreeMap;
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+struct jwt_claims {
+    login_name: String,
+    is_admin: String,
+    iat: i64
+}
+
+
+
 
 pub async fn check_auth_user(login_name: &str, auth_token: &str, is_admin: bool, jwt_key: String) -> Result<(), anyhow::Error> {
 
+
+    println!("CHECK JWT");
     let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_key.as_bytes())?;
 
-    let claims: BTreeMap<String, String> = auth_token.verify_with_key(&key)?;
-    println!("{:?}", claims);
+    let map: jwt_claims = auth_token.verify_with_key(&key).unwrap();
+    // let token: Token<Header, BTreeMap<String, <T>, _> = VerifyWithKey::verify_with_key(auth_token, &key).unwrap();
+
+
+    println!("CHECK JWT ENDING");
+    // println!("{:?}", map::<BTreeMap<std::string::String, _>>);
 
     Ok(())
 }
